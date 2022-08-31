81dce15b21c661dc562143b619c991e5eba8805f
AlejandroRG20 <111157823+AlejandroRG20@users.noreply.github.com>
2022-08-31 17:55:23 -0600

    Add files via upload

---
+++ estructurasdecontrol/Condicionales.java
@@ -0,0 +1,29 @@
+package estructurasdecontrol;
+
+public class Condicionales {
+
+	public static void main(String[] args) {
+		// Condicional if (Simple)
+		
+		byte edad =38;
+		String NOMBRE = "Juan";
+		boolean esUnAvenger = true;
+		
+		if((edad > 0 && edad < 100) || (NOMBRE == "Alejandro" && !esUnAvenger)) {
+			//SE OCUPA ESTO SI LA EXPRECIONES VERDADERA
+			System.out.println(NOMBRE +" tiene " + edad +  " años de edad");
+				
+		}
+		   //Condicional if else if (anidada)
+		
+		if() {
+			System
+		}
+			
+		
+		System.out.println("FIN DEL PROGRAMA");
+
+	}
+}
+
+// ! significa NO a lo que 
---
+++ estructurasdecontrol/Ejemplo1.java
@@ -0,0 +1,36 @@
+package estructurasdecontrol;
+import java.util.Scanner;
+
+public class Ejemplo1 {
+	public static void main(String[] args) {
+      String genero, Sarma;
+      int 	edad;
+      String ar;
+      boolean PortaArma = false;
+      
+      Scanner g = new Scanner (System.in);
+      
+      	System.out.println("Ingrese su Genero");
+      	genero = g.toString();
+      	
+      	System.out.println("Ingrese su edad");
+      	edad = g.nextInt();
+        
+      	System.out.println("¿Usted porta algun Arma?");
+      	ar = g.toString();
+      	
+         if(edad > 18) {
+        	 System.out.println("Pasa");
+         }else if (edad <18) {
+        	 System.out.println("no pasa"); 
+        		 
+         }else if		 
+        	 
+        	
+        	 
+         }
+        	 
+		
+	}
+
+}
